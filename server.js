3ec598faa2bcd984cc747f43a652a7b525032c76
luoken <luozhirong@gmail.com>
2016-11-29 13:44:02 -0800

    more progress

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 
 app.get('/resume',function(req, res){
-    console.log("I received a GET request");
-    
+
     db.resume.find(function(err, docs){
 	console.log(docs);
 	res.json(docs);
